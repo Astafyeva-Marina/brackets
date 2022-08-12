cbb2903678e18e34e6910406d9ec7df0a8420d86
Alexey Sidorov <astafyeva.marina@mail.ru>
2022-08-12 19:15:11 +0400

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsEqual = {} 
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsEqual[bracketsConfig[i][1]] = bracketsConfig[i][0]
+  } // получить [')']:'('
+
+  let stack = []
+
+  // перебрать исходную строку
+  for (let i = 0; i < str.length; i++) {
+    let bracketsPeak = str[i]
+    let bracketsNew = stack[stack.length - 1]
+    // запомнить текущий элемент и верхний в стеке
+    if (stack.length === 0) {
+      stack.push(bracketsPeak ) // записываем первый элемент
+    } else {
+      if (bracketsEqual[bracketsPeak] === bracketsNew) {
+        stack.pop() // если скобка закрывает скобку в стеке - удаляется
+      } else {
+        stack.push(bracketsPeak)
+      } // если скобка не закрывает - добавить в стек
+    }
+  }
+  return stack.length === 0
+  // если стек пустой true, иначе false
 }
